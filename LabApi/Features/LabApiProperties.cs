ccb377265d7ad4ec92ed8d648f178324ce215723
Jesus QC <69375249+Jesus-QC@users.noreply.github.com>
2024-04-01 19:41:56 +0200

    LabApi Properties (#16)

---
+++ LabApi/Features/LabApiProperties.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace LabApi.Features;
+
+/// <summary>
+/// Contains LabAPI properties which can be accessed by plugins.
+///
+/// <para>Those properties are a mix of constants and static fields such as <see cref="CompiledVersion"/>.</para>
+/// </summary>
+public static class LabApiProperties
+{
+    /// <summary>
+    /// The version of the loader, stored during its compilation.
+    ///
+    /// <para>Due to being a constant, the value of this field will always be that of its compilation time version.</para>
+    /// </summary>
+    public const string CompiledVersion = "1.0.0.0";
+    
+    /// <summary>
+    /// Indicates the value of <see cref="CompiledVersion"/> the server is currently using.
+    /// </summary>
+    public static readonly Version CurrentVersion = Version.Parse(CompiledVersion);
+}
