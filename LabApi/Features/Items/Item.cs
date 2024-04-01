291960c26f71285ef6013be5babf3482e1c6c589
Jesus QC <69375249+Jesus-QC@users.noreply.github.com>
2024-04-01 21:23:53 +0200

    Item Wrapper (#24)
    * Items
    
    * Update LabApi/Features/Items/Item.cs
    
    Co-authored-by: Beryl <sebastianfemc@gmail.com>
    
    * Update LabApi/Features/Items/Item.cs
    
    Co-authored-by: Beryl <sebastianfemc@gmail.com>
    
    * Update LabApi/Features/Items/Item.cs
    
    Co-authored-by: Beryl <sebastianfemc@gmail.com>
    
    * Update LabApi/Features/Items/Item.cs
    
    Co-authored-by: Beryl <sebastianfemc@gmail.com>
    
    * Update LabApi/Features/Items/Item.cs
    
    Co-authored-by: Beryl <sebastianfemc@gmail.com>
    
    ---------
    
    Co-authored-by: Beryl <sebastianfemc@gmail.com>

---
+++ LabApi/Features/Items/Item.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using InventorySystem.Items;
+
+namespace LabApi.Features.Items;
+
+/// <summary>
+/// The wrapper representing <see cref="ItemBase">items</see>.
+///
+/// <para>Not to be confused with <see cref="Pickup">item pickups</see>.</para>
+/// </summary>
+public class Item
+{
+    /// <summary>
+    /// Contains all the cached items, accessible through their <see cref="ItemBase"/>.
+    /// </summary>
+    public static Dictionary<ItemBase, Item> Dictionary { get; } = [];
+    
+    /// <summary>
+    /// A reference to all instances of <see cref="Item"/>.
+    /// </summary>
+    public static IReadOnlyCollection<Item> List => Dictionary.Values;
+    
+    /// <summary>
+    /// A private constructor to prevent external instantiation.
+    /// </summary>
+    /// <param name="itemBase">The <see cref="ItemBase"/> of the item.</param>
+    private Item(ItemBase itemBase)
+    {
+        Dictionary.Add(itemBase, this);
+        ItemBase = itemBase;
+    }
+    
+    /// <summary>
+    /// The <see cref="ItemBase"/> of the item.
+    /// </summary>
+    public ItemBase ItemBase { get; }
+    
+    /// <summary>
+    /// Gets the item wrapper from the <see cref="Dictionary"/> or creates a new one if it doesn't exist.
+    /// </summary>
+    /// <param name="itemBase">The <see cref="ItemBase"/> of the item.</param>
+    /// <returns>The requested item.</returns>
+    public static Item Get(ItemBase itemBase) =>
+	        Dictionary.TryGetValue(itemBase, out Item item) ? item : new Item(itemBase);
+}
