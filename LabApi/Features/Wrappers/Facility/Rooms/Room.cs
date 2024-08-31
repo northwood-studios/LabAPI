b4f5ef5c4eb46d7132acb605caf2a508ea6f6236
Michal78900 <75911188+Michal78900@users.noreply.github.com>
2024-08-31 22:12:48 +0200

    Ze rum (#63)
    Co-authored-by: Michal78900 <Michal78900@users.noreply.github.com>

--- LabApi/Features/Wrappers/Facility/Rooms/Room.cs
+++ LabApi/Features/Wrappers/Facility/Rooms/Room.cs
@@ -1,5 +1,10 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using Interactables.Interobjects.DoorUtils;
+using LabApi.Features.Wrappers.Facility.Doors;
+using LabApi.Loader.Features.Misc;
 using MapGeneration;
+using UnityEngine;
 
 namespace LabApi.Features.Wrappers.Facility.Rooms;
 
@@ -8,16 +13,23 @@ namespace LabApi.Features.Wrappers.Facility.Rooms;
 /// </summary>
 public class Room
 {
+    [InitializeWrapper]
+    internal static void Initialize()
+    {
+        RoomIdentifier.OnAdded += (roomIdentifier) => new Room(roomIdentifier);
+        RoomIdentifier.OnRemoved += (roomIdentier) => Dictionary.Remove(roomIdentier);
+    }
+
     /// <summary>
     /// Contains all the cached rooms in the game, accessible through their <see cref="RoomIdentifier"/>.
     /// </summary>
-    public static Dictionary<RoomIdentifier, Room> Dictionary { get; } = [];
+    private static Dictionary<RoomIdentifier, Room> Dictionary { get; } = [];
 
     /// <summary>
     /// A reference to all <see cref="Room"/> instances currently in the game.
     /// </summary>
     public static IReadOnlyCollection<Room> List => Dictionary.Values;
-    
+
     /// <summary>
     /// A private constructor to prevent external instantiation.
     /// </summary>
@@ -25,14 +37,66 @@ public class Room
     private Room(RoomIdentifier roomIdentifier)
     {
         Dictionary.Add(roomIdentifier, this);
-        RoomIdentifier = roomIdentifier;
+        Base = roomIdentifier;
     }
-    
+
     /// <summary>
-    /// The <see cref="RoomIdentifier">identifier</see> of the room.
+    /// The base object.
     /// </summary>
-    public RoomIdentifier RoomIdentifier { get; }
-    
+    public RoomIdentifier Base { get; }
+
+    /// <summary>
+    /// The room's shape.
+    /// </summary>
+
+    public RoomShape Shape => Base.Shape;
+
+    /// <summary>
+    /// The room's name.
+    /// </summary>
+
+    public RoomName Name => Base.Name;
+
+    /// <summary>
+    /// The zone in which this room is located.
+    /// </summary>
+
+    public FacilityZone Zone => Base.Zone;
+
+    /// <summary>
+    /// Gets the doors that are a part of this room.
+    /// </summary>
+    public IEnumerable<Door> Doors
+    {
+        get
+        {
+            if (!DoorVariant.DoorsByRoom.TryGetValue(Base, out HashSet<DoorVariant> doorList))
+                return [];
+
+            return doorList.Select(Door.Get);
+        }
+    }
+
+    /// <summary>
+    /// Gets the room's <see cref="UnityEngine.Transform"/>.
+    /// </summary>
+    public Transform Transform => Base.transform;
+
+    /// <summary>
+    /// Gets the room's <see cref="UnityEngine.GameObject"/>.
+    /// </summary>
+    public GameObject GameObject => Base.gameObject;
+
+    /// <summary>
+    /// Gets the room's position.
+    /// </summary>
+    public Vector3 Position => Transform.position;
+
+    /// <summary>
+    /// Gets the room's rotation.
+    /// </summary>
+    public Quaternion Rotation => Transform.rotation;
+
     /// <summary>
     /// Gets the room wrapper from the <see cref="Dictionary"/>, or creates a new one if it doesn't exist.
     /// </summary>
@@ -40,4 +104,13 @@ public class Room
     /// <returns>The requested room.</returns>
     public static Room Get(RoomIdentifier roomIdentifier) =>
         Dictionary.TryGetValue(roomIdentifier, out Room room) ? room : new Room(roomIdentifier);
+
+    public static IEnumerable<Room> Get(RoomName roomName) =>
+        List.Where(x => x.Name == roomName);
+
+    public static IEnumerable<Room> Get(RoomShape roomShape) =>
+        List.Where(x => x.Shape == roomShape);
+
+    public static IEnumerable<Room> Get(FacilityZone facilityZone) =>
+        List.Where(x => x.Zone == facilityZone);
 }
