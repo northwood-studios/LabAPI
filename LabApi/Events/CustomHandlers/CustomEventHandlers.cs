e66fde19dc1e79019fb46fafc49c6c029d448369
Jesus QC <69375249+Jesus-QC@users.noreply.github.com>
2024-09-18 18:44:28 +0200

    CustomHandlers Propose (#48)
    * Custom Handlers prototype
    
    * Renaming
    
    * naming
    
    * Update LabApi/Events/CustomHandlers/CustomHandlersManager.cs
    
    Co-authored-by: Beryl <10091181+SebasCapo@users.noreply.github.com>
    
    * Update LabApi/Events/CustomHandlers/CustomHandlersManager.cs
    
    Co-authored-by: Beryl <10091181+SebasCapo@users.noreply.github.com>
    
    * Update LabApi/Events/CustomHandlers/CustomEventHandlers.ServerEvents.cs
    
    Co-authored-by: Beryl <10091181+SebasCapo@users.noreply.github.com>
    
    * Update LabApi/Events/CustomHandlers/CustomEventHandlers.cs
    
    Co-authored-by: Beryl <10091181+SebasCapo@users.noreply.github.com>
    
    * Update LabApi/Events/CustomHandlers/CustomHandlersManager.cs
    
    Co-authored-by: Beryl <10091181+SebasCapo@users.noreply.github.com>
    
    * Update LabApi/Events/CustomHandlers/CustomHandlersManager.cs
    
    Co-authored-by: Beryl <10091181+SebasCapo@users.noreply.github.com>
    
    * Update LabApi/Events/CustomHandlers/CustomHandlersManager.cs
    
    Co-authored-by: Beryl <10091181+SebasCapo@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Beryl <10091181+SebasCapo@users.noreply.github.com>

---
+++ LabApi/Events/CustomHandlers/CustomEventHandlers.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace LabApi.Events.CustomHandlers;
+
+/// <summary>
+/// Handles custom logic for any event.
+/// </summary>
+public abstract partial class CustomEventsHandler
+{
+    /// <summary>
+    /// Internal dictionary to store the registered events and their delegates.
+    /// </summary>
+    internal readonly Dictionary<EventInfo, Delegate> InternalEvents = new ();
+}
