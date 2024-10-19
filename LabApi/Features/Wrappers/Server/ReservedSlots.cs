9b54e6cdac0f10493a51f1cf4f0ee642adef2ebd
David <david.sebesta@post.cz>
2024-10-19 17:50:45 +0200

    Whitelist and Reserved Slots wrappers (#118)
    * - Added whitelist wrapper
    - Added reserved slots wrapper
    
    * Push
    
    * The requested changes
    
    * Namespace fix

---
+++ LabApi/Features/Wrappers/Server/ReservedSlots.cs
@@ -0,0 +1,87 @@
+﻿using GameCore;
+using NorthwoodLib.Pools;
+using System.Collections.Generic;
+
+namespace LabApi.Features.Wrappers;
+
+/// <summary>
+/// A static wrapper representing the <see cref="ReservedSlots">reserved slots</see> of the server.
+/// </summary>
+public static class ReservedSlots
+{
+    /// <summary>
+    /// Absolute path to UserIDReservedSlots file.
+    /// </summary>
+    public static string FilePath => ConfigSharing.Paths[3] + "UserIDReservedSlots.txt";
+
+    /// <summary>
+    /// All currently players with reserved slots.
+    /// </summary>
+    public static IEnumerable<string> WhitelistedPlayers => ReservedSlot.Users;
+
+    /// <summary>
+    /// Amount of players with reserved slots.
+    /// </summary>
+    public static int Count => ReservedSlot.Users.Count;
+
+    /// <summary>
+    /// Checks if player has a reserved slot on the server.
+    /// </summary>
+    /// <param name="userId">The user id of the player.</param>
+    /// <returns>Whether player has a reserved slot.</returns>
+    public static bool HasReservedSlot(string userId) => ReservedSlot.HasReservedSlot(userId);
+
+    /// <summary>
+    /// Reloads reserved slots from the file.
+    /// </summary>
+    public static void Reload() => ReservedSlot.Reload();
+
+    /// <summary>
+    /// Gives the player a reserved slot and saves it to file.
+    /// </summary>
+    /// <param name="userId">The userid.</param>
+    public static void Add(string userId)
+    {
+        if (HasReservedSlot(userId))
+            return;
+
+        List<string> lines = FileManager.ReadAllLinesList(FilePath);
+        lines.Add(userId);
+        FileManager.WriteToFile(lines, FilePath, true);
+        ReservedSlot.Users.Add(userId);
+    }
+
+    /// <summary>
+    /// Gives the player a reserved slot and saves it to file.
+    /// </summary>
+    /// <param name="player">The target player.</param>
+    public static void Add(Player player) => Add(player.UserId);
+
+    /// <summary>
+    /// Removes the player's reserved slot and saves it to file.
+    /// </summary>
+    /// <param name="userId">The user id.</param>
+    /// <returns>Whether the user id was found and removed from reserved slots.</returns>
+    public static bool Remove(string userId)
+    {
+        if (!HasReservedSlot(userId))
+            return false;
+
+        List<string> lines = FileManager.ReadAllLinesList(FilePath);
+
+        int result = lines.RemoveAll(n => string.IsNullOrEmpty(n) || n == userId);
+        ReservedSlot.Users.Remove(userId);
+        FileManager.WriteToFile(lines, FilePath, true);
+
+        ListPool<string>.Shared.Return(lines);
+        return result > 0;
+    }
+
+    /// <summary>
+    ///  Removes player from whitelist and saves it to file.
+    /// </summary>
+    /// <param name="player">The target player.</param>
+    /// <returns>Whether the player was found and removed from the reserved slots.</returns>
+    public static bool Remove(Player player) => Remove(player.UserId);
+}
+
---
+++ LabApi/Features/Wrappers/Server/Whitelist.cs
@@ -0,0 +1,102 @@
+﻿using GameCore;
+using NorthwoodLib.Pools;
+using System.Collections.Generic;
+
+namespace LabApi.Features.Wrappers;
+
+/// <summary>
+/// A static wrapper representing the <see cref="WhiteList">whitelist</see> of the server.
+/// </summary>
+public static class Whitelist
+{
+    /// <summary>
+    /// Absolute path to UserIDWhitelist file.
+    /// </summary>
+    public static string FilePath => ConfigSharing.Paths[2] + "UserIDWhitelist.txt";
+
+    /// <summary>
+    /// All currently whitelisted players.
+    /// </summary>
+    public static IEnumerable<string> WhitelistedPlayers => WhiteList.Users;
+
+    /// <summary>
+    /// Amount of whitelisted players.
+    /// </summary>
+    public static int Count => WhiteList.Users.Count;
+
+    /// <summary>
+    /// Gets or sets whether the whitelist is currently enabled. This value is reset to the config one after server restart.
+    /// </summary>
+    public static bool WhitelistEnabled
+    {
+        get => WhiteList.WhitelistEnabled;
+        set => WhiteList.WhitelistEnabled = value;
+    }
+
+    /// <summary>
+    /// Checks if player is on whitelist.
+    /// </summary>
+    /// <param name="userId">The user id of the player.</param>
+    /// <returns>Whether player is on whitelist.</returns>
+    public static bool IsOnWhitelist(string userId) => WhiteList.IsOnWhitelist(userId);
+
+    /// <summary>
+    /// Chhecks if the player is allowed on the server if the whitelist is enabled.
+    /// </summary>
+    /// <param name="userId">The user id of the player.</param>
+    /// <returns>Whether the player is whitelisted. Will always return true if whitelist is disabled.</returns>
+    public static bool IsWhitelisted(string userId) => WhiteList.IsWhitelisted(userId);
+
+    /// <summary>
+    /// Reloads whitelist from the whitelist file.
+    /// </summary>
+    public static void Reload() => WhiteList.Reload();
+
+    /// <summary>
+    /// Adds player to whitelist and saves it to file.
+    /// </summary>
+    /// <param name="userId">The userid.</param>
+    public static void Add(string userId)
+    {
+        if (IsOnWhitelist(userId))
+            return;
+
+        List<string> lines = FileManager.ReadAllLinesList(FilePath);
+        lines.Add(userId);
+        FileManager.WriteToFile(lines, FilePath, true);
+        WhiteList.Users.Add(userId);
+    }
+
+    /// <summary>
+    /// Adds player to whitelist and saves it to file.
+    /// </summary>
+    /// <param name="player">The player to be added onto whitelist.</param>
+    public static void Add(Player player) => Add(player.UserId);
+
+    /// <summary>
+    /// Removes player from whitelist and saves it to file.
+    /// </summary>
+    /// <param name="userId">The user id.</param>
+    /// <returns>Whether the user id was found and removed from the whitelist.</returns>
+    public static bool Remove(string userId)
+    {
+        if (!IsOnWhitelist(userId))
+            return false;
+
+        List<string> lines = FileManager.ReadAllLinesList(FilePath);
+
+        int result = lines.RemoveAll(n => string.IsNullOrEmpty(n) || n == userId);
+        WhiteList.Users.Remove(userId);
+        FileManager.WriteToFile(lines, FilePath, true);
+
+        ListPool<string>.Shared.Return(lines);
+        return result > 0;
+    }
+
+    /// <summary>
+    ///  Removes player from whitelist and saves it to file.
+    /// </summary>
+    /// <param name="player">The player to be removed from the whitelist.</param>
+    /// <returns>Whether the player was found and removed from the whitelist.</returns>
+    public static bool Remove(Player player) => Remove(player.UserId);
+}
