35da33220089aa317de3f80f994ca4aae9b3bb24
Lumi <me@jxtq.moe>
2025-02-13 20:28:32 +0000

    refactor: change GetTeam method

--- LabApi/Features/Extensions/RoleExtensions.cs
+++ LabApi/Features/Extensions/RoleExtensions.cs
@@ -17,16 +17,7 @@ public static class RoleExtensions
     /// </summary>
     /// <param name="roleType">The <see cref="RoleTypeId"/>.</param>
     /// <returns>The <see cref="Team"/>.</returns>
-    public static Team GetTeam(this RoleTypeId roleType) => roleType switch
-    {
-        RoleTypeId.ChaosConscript or RoleTypeId.ChaosMarauder or RoleTypeId.ChaosRepressor or RoleTypeId.ChaosRifleman => Team.ChaosInsurgency,
-        RoleTypeId.Scientist => Team.Scientists,
-        RoleTypeId.ClassD => Team.ClassD,
-        RoleTypeId.Scp049 or RoleTypeId.Scp939 or RoleTypeId.Scp0492 or RoleTypeId.Scp079 or RoleTypeId.Scp096 or RoleTypeId.Scp106 or RoleTypeId.Scp173 or RoleTypeId.Scp3114 => Team.SCPs,
-        RoleTypeId.FacilityGuard or RoleTypeId.NtfCaptain or RoleTypeId.NtfPrivate or RoleTypeId.NtfSergeant or RoleTypeId.NtfSpecialist => Team.FoundationForces,
-        RoleTypeId.Tutorial => Team.OtherAlive,
-        _ => Team.Dead,
-    };
+    public static Team GetTeam(this RoleTypeId roleType) => PlayerRoleLoader.AllRoles.TryGetValue(roleType, out PlayerRoleBase prb) ? prb.Team : Team.OtherAlive;
     
     /// <summary>
     /// Gets the human-readable version of a <see cref="RoleTypeId"/>'s name.
