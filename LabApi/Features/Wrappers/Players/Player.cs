030d5ca37dbe1af88142219295aa2027b6a436ff
brayden-dowson <88667957+brayden-dowson@users.noreply.github.com>
2025-06-27 06:00:08 +0930

    Fixed issue where getting player wrapper before unity Start has been called caused error in AddPlayer (#248)

--- LabApi/Features/Wrappers/Players/Player.cs
+++ LabApi/Features/Wrappers/Players/Player.cs
@@ -844,7 +844,7 @@ public class Player
         if (referenceHub == null)
             return null;
 
-        return Dictionary.TryGetValue(referenceHub, out Player player) ? player : new Player(referenceHub);
+        return Dictionary.TryGetValue(referenceHub, out Player player) ? player : CreatePlayerWrapper(referenceHub);
     }
 
     /// <summary>
@@ -1587,13 +1587,37 @@ public class Player
     }
 
     /// <summary>
+    /// Creates a new wrapper for the player using the player's <see cref="global::ReferenceHub"/>.
+    /// </summary>
+    /// <param name="referenceHub">The <see cref="global::ReferenceHub"/> of the player.</param>
+    /// <returns>The created player wrapper.</returns>
+    private static Player CreatePlayerWrapper(ReferenceHub referenceHub)
+    {
+        Player player = new(referenceHub);
+
+        if (referenceHub.isLocalPlayer)
+            Server.Host = player;
+
+        return player;
+    }
+
+    /// <summary>
     /// Handles the creation of a player in the server.
     /// </summary>
     /// <param name="referenceHub">The reference hub of the player.</param>
     private static void AddPlayer(ReferenceHub referenceHub)
     {
-        if (!referenceHub.isLocalPlayer)
-            _ = new Player(referenceHub);
+        try
+        {
+            if (Dictionary.ContainsKey(referenceHub))
+                return;
+
+            CreatePlayerWrapper(referenceHub);
+        }
+        catch(Exception ex)
+        {
+            Console.Logger.InternalError($"Failed to handle player addition with exception: {ex}");
+        }
     }
 
     /// <summary>
@@ -1602,12 +1626,22 @@ public class Player
     /// <param name="referenceHub">The reference hub of the player.</param>
     private static void RemovePlayer(ReferenceHub referenceHub)
     {
-        if (referenceHub.authManager.UserId != null)
-            UserIdCache.Remove(referenceHub.authManager.UserId);
+        try
+        {
+            if (referenceHub.authManager.UserId != null)
+                UserIdCache.Remove(referenceHub.authManager.UserId);
 
-        if (TryGet(referenceHub.gameObject, out Player? player))
-            CustomDataStoreManager.RemovePlayer(player);
+            if (TryGet(referenceHub.gameObject, out Player? player))
+                CustomDataStoreManager.RemovePlayer(player);
 
-        Dictionary.Remove(referenceHub);
+            if (referenceHub.isLocalPlayer)
+                Server.Host = null;
+
+            Dictionary.Remove(referenceHub);
+        }
+        catch(Exception ex)
+        {
+            Console.Logger.InternalError($"Failed to handle player removal with exception: {ex}");
+        }
     }
 }
