d0d641a0e20cad96af098a1b88331d0d0be57958
Lumi <me@jxtq.moe>
2025-10-03 14:17:16 +0100

    feat: new events

---
+++ LabApi/Events/Arguments/ScpEvents/ScpHumeShieldBroken.cs
@@ -0,0 +1,23 @@
+﻿using LabApi.Events.Arguments.Interfaces;
+using LabApi.Features.Wrappers;
+using System;
+
+namespace LabApi.Events.Arguments.ScpEvents;
+
+/// <summary>
+/// Represents the arguments for the <see cref="Handlers.ScpEvents.ScpHumeShieldBroken"/> event.
+/// </summary>
+public class ScpHumeShieldBroken : EventArgs, IPlayerEvent
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ScpHumeShieldBroken"/> class.
+    /// </summary>
+    /// <param name="player">The player SCP whom hume shield broke.</param>
+    public ScpHumeShieldBroken(ReferenceHub player)
+    {
+        Player = Player.Get(player);
+    }
+
+    /// <inheritdoc/>
+    public Player Player { get; }
+}
