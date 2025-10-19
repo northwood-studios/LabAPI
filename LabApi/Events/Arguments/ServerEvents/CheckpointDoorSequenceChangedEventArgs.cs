56cfd0d10eb401487e3b8d321cd7aad97ddc38b4
SlejmUr <slejmur@protonmail.com>
2025-10-19 23:07:50 +0000

    Add Map events

---
+++ LabApi/Events/Arguments/ServerEvents/CheckpointDoorSequenceChangedEventArgs.cs
@@ -0,0 +1,32 @@
+﻿using LabApi.Features.Wrappers;
+using System;
+using static Interactables.Interobjects.CheckpointDoor;
+
+namespace LabApi.Events.Arguments.ServerEvents;
+
+/// <summary>
+/// Represents the arguments for the <see cref="Handlers.ServerEvents.CheckpointDoorSequenceChanged"/> event.
+/// </summary>
+public class CheckpointDoorSequenceChangedEventArgs : EventArgs
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DoorDamagingEventArgs"/> class.
+    /// </summary>
+    /// <param name="door">The current checkpoint door.</param>
+    /// <param name="sequenceState">The current state this checkpoint is.</param>
+    public CheckpointDoorSequenceChangedEventArgs(Interactables.Interobjects.CheckpointDoor door, SequenceState sequenceState)
+    {
+        CheckpointDoor = CheckpointDoor.Get(door);
+        CurrentSequence = sequenceState;
+    }
+
+    /// <summary>
+    /// Gets the current Checkpoint Door.
+    /// </summary>
+    public CheckpointDoor CheckpointDoor { get; }
+
+    /// <summary>
+    /// Gets the damage value.
+    /// </summary>
+    public SequenceState CurrentSequence { get; }
+}
---
+++ LabApi/Events/Arguments/ServerEvents/RoomColorChangedEventArgs.cs
@@ -0,0 +1,33 @@
+﻿using LabApi.Features.Wrappers;
+using MapGeneration;
+using System;
+using UnityEngine;
+
+namespace LabApi.Events.Arguments.ServerEvents;
+
+/// <summary>
+/// Represents the arguments for the <see cref="Handlers.ServerEvents.RoomColorChanged"/> event.
+/// </summary>
+public class RoomColorChangedEventArgs : EventArgs
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="RoomColorChangedEventArgs"/> class.
+    /// </summary>
+    /// <param name="room">The room that is changing its color.</param>
+    /// <param name="newState">The <paramref name="room"/>'s new color.</param>
+    public RoomColorChangedEventArgs(RoomIdentifier room, Color newState)
+    {
+        Room = Room.Get(room);
+        NewState = newState;
+    }
+
+    /// <summary>
+    /// Gets the current room.
+    /// </summary>
+    public Room Room { get; }
+
+    /// <summary>
+    /// Gets the <see cref="Room"/> new color.
+    /// </summary>
+    public Color NewState { get; }
+}
