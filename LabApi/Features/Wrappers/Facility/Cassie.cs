bae22a838e230724f5f722f119f3174de11dfd56
SlejmUr <slejmur@protonmail.com>
2026-01-10 08:19:57 +0000

    Rename Cassie to Announcer

--- LabApi/Features/Wrappers/Facility/Cassie.cs
+++ LabApi/Features/Wrappers/Facility/Cassie.cs
@@ -1,253 +1,101 @@
 ﻿using Cassie;
-using Cassie.Interpreters;
 using PlayerRoles;
 using PlayerStatsSystem;
-using Respawning.NamingRules;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace LabApi.Features.Wrappers;
 
-/// <summary>
-/// The wrapper for in game CASSIE announcer.
-/// </summary>
+/// <inheritdoc cref="Announcer"/>
+[Obsolete("Use Announcer instead of Cassie.", true)]
 public static class Cassie
 {
-    /// <summary>
-    /// Gets whether CASSIE is currently speaking.
-    /// </summary>
-    public static bool IsSpeaking => CassieAnnouncementDispatcher.CurrentAnnouncement != null;
+    /// <inheritdoc cref="Announcer.IsSpeaking"/>
+    [Obsolete("Use Announcer.IsSpeaking instead.", true)]
+    public static bool IsSpeaking
+        => Announcer.IsSpeaking;
 
-    /// <summary>
-    /// Gets all available voice lines for CASSIE.
-    /// </summary>
-    public static CassieLine[] AllLines => !CassieTtsAnnouncer.TryGetDatabase(out CassieLineDatabase db) ? [] : db.AllLines;
+    /// <inheritdoc cref="Announcer.LineDatabase"/>
+    [Obsolete("Use Announcer.LineDatabase instead.", true)]
+    public static CassieLineDatabase? LineDatabase
+        => Announcer.LineDatabase;
 
-    /// <summary>
-    /// Gets the line database for CASSIE.
-    /// </summary>
-    public static CassieLineDatabase? LineDatabase => !CassieTtsAnnouncer.TryGetDatabase(out CassieLineDatabase db) ? null : db;
+    /// <inheritdoc cref="Announcer.AllLines"/>
+    [Obsolete("Use Announcer.AllLines instead.", true)]
+    public static CassieLine[] AllLines
+        => Announcer.AllLines;
 
-    /// <summary>
-    /// Gets all collection names in which voice lines are in.
-    /// </summary>
-    public static string[] CollectionNames => AllLines.Select(n => n.ApiName).Distinct().ToArray();
+    /// <inheritdoc cref="Announcer.CollectionNames"/>
+    [Obsolete("Use Announcer.CollectionNames instead.", true)]
+    public static string[] CollectionNames
+        => Announcer.CollectionNames;
 
-    /// <summary>
-    /// Checks whether a specified word is valid for CASSIE.
-    /// <note>String comparison is case-insensitive.</note>
-    /// </summary>
-    /// <param name="word">The word to check.</param>
-    /// <returns>Whether the word is valid.</returns>
-    public static bool IsValid(string word) => CollectionNames.Any(line => line.Equals(word, StringComparison.InvariantCultureIgnoreCase));
+    /// <inheritdoc cref="Announcer.IsValid(string)"/>
+    [Obsolete("Use Announcer.IsValid(string) instead.", true)]
+    public static bool IsValid(string word)
+        => Announcer.IsValid(word);
 
-    /// <summary>
-    /// Calculates duration of specific message.
-    /// </summary>
-    /// <param name="message">The message.</param>
-    /// <param name="rawNumber">Raw numbers.</param>
-    /// <param name="speed">The speed of the cassie talking.</param>
-    /// <returns>Duration of the specific message in seconds.</returns>
-    [Obsolete("Use CalculateDuration(string message, CassiePlaybackModifiers playbackModifiers) instead.", true)]
+    /// <inheritdoc cref="Announcer.CalculateDuration(string, bool, float)"/>
+    [Obsolete("Use Announcer.CalculateDuration(string message, CassiePlaybackModifiers playbackModifiers) instead.", true)]
     public static float CalculateDuration(string message, bool rawNumber = false, float speed = 1f)
-    {
-        CassiePlaybackModifiers playbackModifiers = new()
-        {
-            Pitch = speed,
-        };
-        return (float)CalculateDuration(message, playbackModifiers);
-    }
+        => Announcer.CalculateDuration(message, rawNumber, speed);
 
-    /// <summary>
-    /// Calculates duration of specific message.
-    /// </summary>
-    /// <param name="message">The message.</param>
-    /// <param name="playbackModifiers">The playback modifier.</param>
-    /// <returns>Duration of the specific message in seconds.</returns>
+    /// <inheritdoc cref="Announcer.CalculateDuration(string, CassiePlaybackModifiers)"/>
+    [Obsolete("Use Announcer.CalculateDuration(string, CassiePlaybackModifiers) instead.", true)]
     public static double CalculateDuration(string message, CassiePlaybackModifiers playbackModifiers)
-    {
-        CalculateDuration(message, playbackModifiers, out var time);
-        return time;
-    }
+        => Announcer.CalculateDuration(message, playbackModifiers);
 
-    /// <summary>
-    /// Plays a custom announcement.
-    /// Queues a custom announcement.
-    /// </summary>
-    /// <param name="message">The sentence CASSIE is supposed to say.</param>
-    /// <param name="isHeld">Sets a minimal 3-second moment of silence before the announcement. For most cases you wanna keep it true.</param>
-    /// <param name="isNoisy">Whether the background noises play.</param>
-    /// <param name="isSubtitles">Show subtitles.</param>
-    /// <param name="customSubtitles">Custom subtitles to appear instead of the actual message.</param>
-    [Obsolete("Use Message(string message, string customSubtitles = \"\", bool playBackground = true, float priority = 0f, float glitchScale = 1f) instead.", true)]
+    /// <inheritdoc cref="Announcer.Message(string, bool, bool, bool, string)"/>
+    [Obsolete("Use Announcer.Message(string message, string customSubtitles = \"\", bool playBackground = true, float priority = 0f, float glitchScale = 1f) instead.", true)]
     public static void Message(string message, bool isHeld = false, bool isNoisy = true, bool isSubtitles = true, string customSubtitles = "")
-        => Message(message, customSubtitles);
+        => Announcer.Message(message, customSubtitles);
 
-    /// <summary>
-    /// Queues a custom announcement.
-    /// </summary>
-    /// <param name="message">The sentence CASSIE is supposed to say.</param>
-    /// <param name="customSubtitles">Custom subtitles to play.</param>
-    /// <param name="playBackground">Should play the background track (bells, noise).</param>
-    /// <param name="priority">The priority of this message.</param>
-    /// <param name="glitchScale">Intensity of glitches and stutters added before sending to clients.</param>
+    /// <inheritdoc cref="Announcer.Message(string, string, bool, float, float)"/>
+    [Obsolete("Use Announcer.Message(string, string, bool, float, float) instead.", true)]
     public static void Message(string message, string customSubtitles = "", bool playBackground = true, float priority = 0f, float glitchScale = 1f)
-    {
-        CassieTtsPayload payload = new(message, customSubtitles, playBackground);
-        Message(payload, priority, glitchScale);
-    }
+        => Announcer.Message(message, customSubtitles, playBackground, priority, glitchScale);
 
-    /// <summary>
-    /// Queues an cassie payload announcement.
-    /// </summary>
-    /// <param name="payload">The payload to sent.</param>
-    /// <param name="priority">The priority of this message.</param>
-    /// <param name="glitchScale">Intensity of glitches and stutters added before sending to clients.</param>
+    /// <inheritdoc cref="Announcer.Message(CassieTtsPayload, float, float)"/>
+    [Obsolete("Use Announcer.Message(CassieTtsPayload, float, float) instead.", true)]
     public static void Message(CassieTtsPayload payload, float priority = 0f, float glitchScale = 1f)
-    {
-        CassieAnnouncement announcement = new(payload, priority, glitchScale);
-        CassieAnnouncementDispatcher.AddToQueue(announcement);
-    }
+        => Announcer.Message(payload, priority, glitchScale);
 
-    /// <summary>
-    /// Plays the custom announcement with chance of 0f to 1f of adding a glitch or jam before each word. Values closer to 1f are higher chances.
-    /// </summary>
-    /// <param name="message">The sentence CASSIE is supposed to say.</param>
-    /// <param name="glitchChance">The chance for glitch sound to be added before each word. Range from 0f to 1f.</param>
-    /// <param name="jamChance">The chance for jam sound to be added before each word. Range from 0f to 1f.</param>
-    public static void GlitchyMessage(string message, float glitchChance, float jamChance) => CassieGlitchifier.Glitchify(message, glitchChance, jamChance);
+    /// <inheritdoc cref="Announcer.GlitchyMessage(string, float, float)"/>
+    [Obsolete("Use Announcer.GlitchyMessage(string, float, float) instead.", true)]
+    public static void GlitchyMessage(string message, float glitchChance, float jamChance)
+        => Announcer.GlitchyMessage(message, glitchChance, jamChance);
 
-    /// <summary>
-    /// Plays the termination announcement of a SCP player. If the specified player does not have an SCP role then nothing is played.
-    /// </summary>
-    /// <param name="player">The player who is being terminated as an SCP.</param>
-    /// <param name="info">Damage handler causing the death of the player.</param>
-    public static void ScpTermination(Player player, DamageHandlerBase info) => CassieScpTerminationAnnouncement.AnnounceScpTermination(player.ReferenceHub, info);
+    /// <inheritdoc cref="Announcer.ScpTermination(Player, DamageHandlerBase)"/>
+    [Obsolete("Use Announcer.ScpTermination(Player, DamageHandlerBase) instead.", true)]
+    public static void ScpTermination(Player player, DamageHandlerBase info)
+        => Announcer.ScpTermination(player, info);
 
-    /// <summary>
-    /// Clears the CASSIE announcements queue.
-    /// </summary>
-    public static void Clear() => CassieAnnouncementDispatcher.ClearAll();
+    /// <inheritdoc cref="Announcer.Clear()"/>
+    [Obsolete("Use Announcer.Clear() instead.", true)]
+    public static void Clear()
+        => Announcer.Clear();
 
-    /// <summary>
-    /// Converts player's team into CASSIE-able word. Unit names are converted into NATO_X words, followed by a number. For example "Alpha-5" is converted to "NATO_A 5".
-    /// </summary>
-    /// <param name="team">Target team.</param>
-    /// <param name="unitName">MTF Unit name (for team <see cref="Team.FoundationForces"/>).</param>
-    /// <returns>Converted name.</returns>
+    /// <inheritdoc cref="Announcer.ConvertTeam(Team, string)"/>
+    [Obsolete("Use Announcer.ConvertTeam(Team, string) instead.", true)]
     public static string ConvertTeam(Team team, string unitName)
-    {
-        string text = "CONTAINMENTUNIT UNKNOWN";
-        switch (team)
-        {
-            case Team.FoundationForces:
-                {
-                    if (!NamingRulesManager.TryGetNamingRule(team, out UnitNamingRule unitNamingRule))
-                    {
-                        return text;
-                    }
+        => Announcer.ConvertTeam(team, unitName);
 
-                    string text2 = unitNamingRule.TranslateToCassie(unitName);
-                    return "CONTAINMENTUNIT " + text2;
-                }
-
-            case Team.ChaosInsurgency:
-                return "BY CHAOSINSURGENCY";
-            case Team.Scientists:
-                return "BY SCIENCE PERSONNEL";
-            case Team.ClassD:
-                return "BY CLASSD PERSONNEL";
-            default:
-                return text;
-        }
-    }
-
-    /// <summary>
-    /// Converts number into string.
-    /// </summary>
-    /// <param name="num">The number.</param>
-    /// <returns>Number converted to string.</returns>
+    /// <inheritdoc cref="Announcer.ConvertNumber(int)"/>
+    [Obsolete("Use Announcer.ConvertNumber(int) instead.", true)]
     public static string ConvertNumber(int num)
-    {
-        if (LineDatabase == null)
-        {
-            return string.Empty;
-        }
+        => Announcer.ConvertNumber(num);
 
-        NumberInterpreter? numberInterpreter = (NumberInterpreter?)CassieTtsAnnouncer.Interpreters.FirstOrDefault(x => x is NumberInterpreter);
-        if (numberInterpreter == null)
-        {
-            return string.Empty;
-        }
+    /// <inheritdoc cref="Announcer.ConvertScp(RoleTypeId, out string, out string)"/>
+    [Obsolete("Use Announcer.ConvertScp(RoleTypeId, out string, out string) instead.", true)]
+    public static void ConvertScp(RoleTypeId role, out string withoutSpace, out string withSpace)
+        => Announcer.ConvertScp(role, out withoutSpace, out withSpace);
 
-        string word = num.ToString();
+    /// <inheritdoc cref="Announcer.ConvertScp(string, out string, out string)"/>
+    [Obsolete("Use Announcer.ConvertScp(string, out string, out string) instead.", true)]
+    public static void ConvertScp(string roleName, out string withoutSpace, out string withSpace)
+        => Announcer.ConvertScp(roleName, out withoutSpace, out withSpace);
 
-        CassiePlaybackModifiers cassiePlaybackModifiers = default;
-        StringBuilder sb = new();
-        numberInterpreter.GetResults(LineDatabase, ref cassiePlaybackModifiers, word, sb, out bool halt);
-
-        return sb.ToString();
-    }
-
-    /// <summary>
-    /// Converts player's <see cref="RoleTypeId"/> into an SCP <b>number</b> identifier.
-    /// </summary>
-    /// <param name="role">The target <see cref="RoleTypeId"/>.</param>
-    /// <param name="withoutSpace">The SCP number without spaces between. Used by CASSIE.</param>
-    /// <param name="withSpace">The SCP number with spaces between. Used by Subtitles.</param>
-    public static void ConvertScp(RoleTypeId role, out string withoutSpace, out string withSpace) => CassieScpTerminationAnnouncement.ConvertSCP(role, out withoutSpace, out withSpace);
-
-    /// <summary>
-    /// Converts player's role name into an SCP <b>number</b> identifier.
-    /// </summary>
-    /// <param name="roleName">The targets role name.</param>
-    /// <param name="withoutSpace">The SCP number without spaces between. Used by CASSIE.</param>
-    /// <param name="withSpace">The SCP number with spaces between. Used by Subtitles.</param>
-    public static void ConvertScp(string roleName, out string withoutSpace, out string withSpace) => CassieScpTerminationAnnouncement.ConvertSCP(roleName, out withoutSpace, out withSpace);
-
-    /// <summary>
-    /// Calculates duration of message.
-    /// </summary>
-    /// <param name="remaining">The remaining message.</param>
-    /// <param name="modifiers">The playback modifier.</param>
-    /// <param name="time">The duration of the message.</param>
+    /// <inheritdoc cref="Announcer.CalculateDuration(ReadOnlySpan{char}, CassiePlaybackModifiers, out double)"/>
+    [Obsolete("Use Announcer.CalculateDuration(ReadOnlySpan{char}, CassiePlaybackModifiers, out double) instead.", true)]
     public static void CalculateDuration(ReadOnlySpan<char> remaining, CassiePlaybackModifiers modifiers, out double time)
-    {
-        time = 0;
-
-        if (LineDatabase == null)
-        {
-            return;
-        }
-
-        int index = remaining.IndexOf(' ');
-
-        StringBuilder sb = new();
-
-        ReadOnlySpan<char> word = index < 0 ? remaining : remaining[..index];
-
-        foreach (CassieInterpreter inter in CassieTtsAnnouncer.Interpreters)
-        {
-            List<CassieInterpreter.Result> results = inter.GetResults(LineDatabase, ref modifiers, word, sb, out bool jobDone);
-
-            time += results.Sum(result => result.Modifiers.GetTimeUntilNextWord(result.Line));
-
-            if (jobDone)
-            {
-                // The interpreter claims there's no need to process the word by other interpreters.
-                break;
-            }
-        }
-
-        if (index < 0)
-        {
-            return;
-        }
-
-        remaining = remaining[(index + 1)..];
-        CalculateDuration(remaining, modifiers, out double timeOut);
-        time += timeOut;
-    }
+        => Announcer.CalculateDuration(remaining, modifiers, out time);
 }
