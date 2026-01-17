e464110ca607ee5792ef734c4330a7b42e19194b
TtroubleTT <121741230+TtroubleTT@users.noreply.github.com>
2026-01-17 03:05:32 -0800

    Fix player events interfaces

--- LabApi/Events/Arguments/PlayerEvents/PlayerEffectUpdatingEventArgs.cs
+++ LabApi/Events/Arguments/PlayerEvents/PlayerEffectUpdatingEventArgs.cs
@@ -8,7 +8,7 @@ namespace LabApi.Events.Arguments.PlayerEvents;
 /// <summary>
 /// Represents the arguments for the <see cref="Handlers.PlayerEvents.UpdatingEffect"/> event.
 /// </summary>
-public class PlayerEffectUpdatingEventArgs : EventArgs, ICancellableEvent
+public class PlayerEffectUpdatingEventArgs : EventArgs, ICancellableEvent, IPlayerEvent
 {
     /// <summary>
     /// Initializes a new instance of the <see cref="PlayerEffectUpdatingEventArgs"/> class.
