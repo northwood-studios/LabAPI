7afab9f13bde360c03cfe50cb5bd365736d3c7f0
OMEGA3065 <martin@salstar.sk>
2026-09-02 06:38:57 +0200

    fix: make IsRuntime in PermissionGroup settable by user on-init.

--- LabApi/Features/Permissions/Providers/DefaultPermissionsProvider.cs
+++ LabApi/Features/Permissions/Providers/DefaultPermissionsProvider.cs
@@ -47,10 +47,6 @@ public class DefaultPermissionsProvider : IPermissionsProvider
         {
             // We deserialize the permissions from the file.
             _permissionsDictionary = YamlParser.Deserializer.Deserialize<Dictionary<string, PermissionGroup>>(File.ReadAllText(_permissions.FullName));
-            foreach (PermissionGroup permissionGroup in _permissionsDictionary.Values)
-            {
-                permissionGroup.IsRuntime = false;
-            }
 
             // We then reload the permissions to fill the special permissions.
             ReloadPermissions();
